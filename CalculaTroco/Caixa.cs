45da6f74480937cacb8fa21908850aee893fe29c
Eduardo Estevão <eduardo.estevao.ext@pdg.com.br>
2018-09-13 18:41:12 -0300

    Primeiro commit

---
+++ CalculaTroco/Caixa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaTroco
+{
+    class Caixa
+    {
+        public double Entrada { get; set; }
+        public double Pagamento { get; set; }
+        public double Troco { get; set; }
+        public double Moedas { get; set; }
+
+        public double CalculaTroco(double pagamento)
+        {
+            Troco = pagamento - Entrada;
+
+            return Troco;
+        }
+
+       public string ContaNotas()
+        {
+            double contaTroco = Troco;
+            
+            while (contaTroco >= 5){
+                if (contaTroco >= 50)
+                {
+                    contaTroco -= 50;
+                    Console.Write("50, ");
+
+                }else if(contaTroco >= 20)
+                {
+                    contaTroco -= 20;
+                    Console.Write("20, ");
+                }
+                else if (contaTroco >= 10)
+                {
+                    contaTroco -= 10;
+                    Console.Write("10, ");
+                }
+                else if (Troco >= 5)
+                {
+                    contaTroco -= 5;
+                    Console.Write("5");
+
+                    Moedas = contaTroco;
+
+                    Console.Write(" e " + this.Moedas + " Centavos");
+                }
+               
+            }
+
+            return "";
+
+        }
+        
+    }
+}
--- CalculaTroco/Program.cs
+++ CalculaTroco/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace CalculaTroco
     {
         static void Main(string[] args)
         {
+            Caixa c = new Caixa();
+
+            Console.Write("Digite o valor da compra: ");
+            c.Entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o valor para pagar: ");
+            c.Pagamento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+           
+
+            c.Troco = c.CalculaTroco(c.Pagamento);
+
+            Console.WriteLine("O troco é: " + c.Troco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Write("Notas disponíveis para troco: ");
+            c.ContaNotas();
+            
+
+            Console.ReadLine();
         }
     }
 }
